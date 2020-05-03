7a9843254ebeeb3b63c795f1431d19d10250773f
Ted <ted.cater@acresoftware.com>
2020-05-03 15:26:13 +0100

    Fix broken pause toggling

--- js/index.js
+++ js/index.js
@@ -1,90 +1,95 @@
 const { ipcRenderer } = require("electron");
 
+let channel = 0;
 const streams = [
   "https://stream-relay-geo.ntslive.net/stream?client=NTSWebApp",
-  "https://stream-relay-geo.ntslive.net/stream2?client=NTSWebApp"
+  "https://stream-relay-geo.ntslive.net/stream2?client=NTSWebApp",
 ];
 
-const streamData = [{}, {}];
+// const streamData = [{}, {}];
+// const locationSpan = document.querySelector("#location");
+// const channelSpan = document.querySelector("#channel");
+// const timeSpan = document.querySelector("#time");
+// const nameSpan = document.querySelector("#name");
 
-const channelSpan = document.querySelector("#channel");
-const locationSpan = document.querySelector("#location");
-const timeSpan = document.querySelector("#time");
+const audioPlayer = document.getElementById("audio-player");
 
-let audio = new Audio(streams[0]);
-let paused = true;
-let channel = 0;
-
-ipcRenderer.on("togglePause", () => {
-  if (paused) {
-    paused = false;
-    audio = new Audio(streams[channel]);
-    audio.play();
+ipcRenderer.on("togglepause", () => {
+  if (audioPlayer.paused) {
+    // Play
+    audioPlayer.setAttribute("src", streams[channel]);
+    audioPlayer.load();
+    audioPlayer.play();
   } else {
-    paused = true;
-    audio.pause();
-    delete audio;
+    // Pause
+    audioPlayer.setAttribute("src", "");
+    audioPlayer.pause();
   }
 });
 
-ipcRenderer.on("switchChannels", () => {
-  channel ^= 1;
-  updateUI(channel);
+// ipcRenderer.on("switchchannels", () => {
+//   channel ^= 1;
+//   updateUI(channel);
 
-  audio.pause();
-  delete audio;
+//   audio.pause();
+//   delete audio;
 
-  audio = new Audio(streams[channel]);
-  audio.play();
-  paused = false;
-});
+//   audio = new Audio(streams[channel]);
+//   audio.play();
+//   paused = false;
+// });
 
-window.addEventListener("load", () => {
-  const headers = new Headers();
-  headers.append("pragma", "no-cache");
-  headers.append("cache-control", "no-cache");
+// window.addEventListener("load", () => {
+//   // const headers = new Headers();
+//   // headers.append("pragma", "no-cache");
+//   // headers.append("cache-control", "no-cache");
 
-  fetch("https://www.nts.live/api/v2/live", { headers })
-    .then(res => res.json())
-    .then(data => {
-      streamData[0] = apiToStreamData(data, 0);
-      streamData[1] = apiToStreamData(data, 1);
-      updateUI(channel);
-    });
-});
+//   // console.log("load");
 
-const updateUI = channel => {
-  channelSpan.innerText = `${channel + 1}`;
-  locationSpan.innerText = streamData[channel].location;
-  timeSpan.innerText = streamData[channel].time;
-  document.body.style.backgroundImage = `url('${
-    streamData[channel].background
-  }')`;
-};
+//   // var myHeaders = new Headers();
+//   // myHeaders.append("pragma", "no-cache");
+//   // myHeaders.append("cache-control", "no-cache");
 
-const apiToStreamData = (data, channel) => {
-  const { start_timestamp, end_timestamp } = data.results[channel].now;
+//   fetch("https://www.nts.live/api/v2/live", { cache: "no-store" })
+//     .then((res) => res.json())
+//     .then((data) => {
+//       streamData[0] = apiToStreamData(data, 0);
+//       streamData[1] = apiToStreamData(data, 1);
+//       updateUI(channel);
+//     });
+// });
 
-  const start = new Date(start_timestamp);
-  const end = new Date(end_timestamp);
-  const startTimeString = start.toLocaleTimeString("en-GB", {
-    hour: "2-digit",
-    minute: "2-digit"
-  });
-  const endTimeString = end.toLocaleTimeString("en-GB", {
-    hour: "2-digit",
-    minute: "2-digit"
-  });
-  const time = `${startTimeString} - ${endTimeString}`;
+// const updateUI = (channel) => {
+//   channelSpan.innerText = `${channel + 1}`;
+//   locationSpan.innerText = streamData[channel].location;
+//   timeSpan.innerText = streamData[channel].time;
+//   nameSpan.innerHTML = streamData[channel].name;
+//   document.body.style.backgroundImage = `url('${streamData[channel].background}')`;
+// };
 
-  const { name, description, media, location_long } = data.results[
-    channel
-  ].now.embeds.details;
-  return {
-    name,
-    time,
-    description,
-    location: location_long,
-    background: media.background_medium
-  };
-};
+// const apiToStreamData = (data, channel) => {
+//   const { start_timestamp, end_timestamp } = data.results[channel].now;
+
+//   const start = new Date(start_timestamp);
+//   const end = new Date(end_timestamp);
+//   const startTimeString = start.toLocaleTimeString("en-GB", {
+//     hour: "2-digit",
+//     minute: "2-digit",
+//   });
+//   const endTimeString = end.toLocaleTimeString("en-GB", {
+//     hour: "2-digit",
+//     minute: "2-digit",
+//   });
+//   const time = `${startTimeString} - ${endTimeString}`;
+
+//   const { name, description, media, location_long } = data.results[
+//     channel
+//   ].now.embeds.details;
+//   return {
+//     name,
+//     time,
+//     description,
+//     location: location_long,
+//     background: media.background_medium,
+//   };
+// };
--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const {
   Tray,
   nativeImage,
   BrowserWindow,
-  globalShortcut
+  globalShortcut,
 } = require("electron");
 const path = require("path");
 
@@ -14,7 +14,7 @@ app.on("ready", () => {
   // Initialise tray
   const icon = nativeImage.createFromDataURL(base64Icon);
   tray = new Tray(icon);
-  tray.on("click", function(event) {
+  tray.on("click", function (event) {
     toggleWindow();
 
     // Show devtools when command clicked
@@ -25,17 +25,17 @@ app.on("ready", () => {
 
   // Initialise menu
   window = new BrowserWindow({
-    width: 320,
-    height: 240,
-    // width: 550,
-    // height: 400,
+    // width: 320,
+    // height: 240,
+    width: 1100,
+    height: 800,
     show: false,
     frame: false,
     resizable: false,
     alwaysOnTop: true,
     webPreferences: {
-      nodeIntegration: true
-    }
+      nodeIntegration: true,
+    },
   });
 
   // Ensure window appears on top of fullscreen applications
@@ -55,12 +55,12 @@ app.on("ready", () => {
   // Configure keyboard shortcuts
   globalShortcut.register("CommandOrControl+P", () => {
     console.log("Command: Toggle pause");
-    window.webContents.send("togglePause");
+    window.webContents.send("togglepause");
   });
 
   globalShortcut.register("CommandOrControl+L", () => {
     console.log("Command: Switch channels");
-    window.webContents.send("switchChannels");
+    window.webContents.send("switchchannels");
   });
 });
 
